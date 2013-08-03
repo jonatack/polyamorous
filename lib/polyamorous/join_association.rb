27807a9ca4d5e76c7e2f0719a2677abb9ce5c0e3
jonatack <jonnyatack@gmail.com>
2013-08-03 17:08:13 +0200

    Compatibility with Rails commit 743b675 of July 16, 2013
    https://github.com/rails/rails/commit/743b67508e2027e1d086142ccbec47a19f
    c943f6

--- lib/polyamorous/join_association.rb
+++ lib/polyamorous/join_association.rb
@@ -40,14 +40,14 @@ module Polyamorous
       equality_without_polymorphism(other) && base_klass == other.base_klass
     end
 
-    def build_constraint_with_polymorphism(reflection, table, key, foreign_table, foreign_key)
-      if reflection.options[:polymorphic]
-        build_constraint_without_polymorphism(reflection, table, key, foreign_table, foreign_key).
-        and(foreign_table[reflection.foreign_type].
-        eq(reflection.klass.name)
+    def build_constraint_with_polymorphism(klass, table, key, foreign_table, foreign_key)
+      if @reflection.options[:polymorphic]
+        build_constraint_without_polymorphism(klass, table, key, foreign_table, foreign_key).
+        and(foreign_table[@reflection.foreign_type].
+        eq(klass.name)
         )
       else
-        build_constraint_without_polymorphism(reflection, table, key, foreign_table, foreign_key)
+        build_constraint_without_polymorphism(klass, table, key, foreign_table, foreign_key)
       end
     end
 
